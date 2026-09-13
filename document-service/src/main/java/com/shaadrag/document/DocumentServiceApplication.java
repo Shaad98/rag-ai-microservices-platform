892f82aad57ca846f01fd887f9deb24da5f89a93
Shaad98 <shaadbangi98@gmail.com>
2026-09-13 10:54:51 +0530

    feat(document-service): enable Feign client and Eureka registration

--- document-service/src/main/java/com/shaadrag/document/DocumentServiceApplication.java
+++ document-service/src/main/java/com/shaadrag/document/DocumentServiceApplication.java
@@ -2,8 +2,13 @@ package com.shaadrag.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients 
 
 @SpringBootApplication
+
 public class DocumentServiceApplication {
 
 	public static void main(String[] args) {
